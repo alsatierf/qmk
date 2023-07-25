b8cf77b642d432e96dced2e1b4b03606646f0aa7
Anderson Freitas <alsatierf@gmail.com>
2023-07-25 01:07:20 -0400

    [annepro2] new set of configs
    - Use https://github.com/qmk/qmk_firmware/pull/19449 to fix Alt as OSM
      over RDP.
    - SPC + LSFT on base layer
    - OSM for CGAS + arrows on layer 1
    - OSM activated by combos in any layer
    - Use autoshift to deal with Ç and KC_GRV on layer 1

--- keyboards/annepro2/keymaps/alsatierf/keymap.c
+++ keyboards/annepro2/keymaps/alsatierf/keymap.c
@@ -2,7 +2,6 @@
 #include "annepro2.h"
 #include "config.h"
 #include "g/keymap_combo.h"
-#include "features/layer_lock.h"
 #include QMK_KEYBOARD_H
 
 enum anne_pro_layers {
@@ -10,25 +9,21 @@ enum anne_pro_layers {
   _FN1_LAYER,
 };
 
-enum custom_keycodes {
-  LLOCK = SAFE_RANGE,
-};
-
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE_LAYER] = LAYOUT_60_ansi( /* Base */
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-    MT(MOD_RALT, KC_GRV), MT(MOD_LSFT, KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, MT(MOD_RSFT, KC_SCLN), KC_QUOT, KC_ENT,
-    MT(MOD_LSFT, KC_NUBS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RCTL, KC_INT1),
-    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN1_LAYER, KC_SPC), KC_RALT, KC_RGUI, KC_RCTL, KC_NO
+    OSL(_FN1_LAYER), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    MT(MOD_LSFT, KC_NUBS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_INT1),
+    KC_LCTL, KC_LGUI, KC_LALT, MT(MOD_LSFT, KC_SPC), KC_RALT, KC_RGUI, KC_RCTL, KC_NUBS
 
   ),
   [_FN1_LAYER] = LAYOUT_60_ansi(
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, OSM(MOD_LSFT), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, LLOCK, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LCTL), KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, RALT(KC_COMM), KC_GRV, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_BASE_LAYER), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 
@@ -36,9 +31,7 @@ const uint16_t keymaps_size = sizeof(keymaps);
 
 void matrix_init_user(void) {}
 
-void matrix_scan_user(void) {
-  layer_lock_task();
-}
+void matrix_scan_user(void) {}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
@@ -52,7 +45,38 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_layer_lock(keycode, record, LLOCK)) { return false; }
-    return true;
-};
+bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case RALT(KC_COMM):
+            return true;
+        default:
+            return false;
+    }
+}
+
+void autoshift_press_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
+    switch(keycode) {
+        case RALT(KC_COMM):
+            register_code16((!shifted) ? RALT(KC_COMM) : RALT(S(KC_COMM)));
+            break;
+        default:
+            if (shifted) {
+                add_weak_mods(MOD_BIT(KC_LSFT));
+            }
+            // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
+            register_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+    }
+}
+
+void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
+    switch(keycode) {
+        case RALT(KC_COMM):
+            unregister_code16((!shifted) ? RALT(KC_COMM) : RALT(S(KC_COMM)));
+            break;
+        default:
+            // & 0xFF gets the Tap key for Tap Holds, required when using Retro Shift
+            // The IS_RETRO check isn't really necessary here, always using
+            // keycode & 0xFF would be fine.
+            unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
+    }
+}
--- keyboards/annepro2/keymaps/alsatierf/rules.mk
+++ keyboards/annepro2/keymaps/alsatierf/rules.mk
@@ -3,4 +3,3 @@ COMBO_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 
 VPATH += keyboards/gboards
-SRC += features/layer_lock.c
